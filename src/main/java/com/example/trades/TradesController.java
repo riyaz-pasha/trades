5cca3c1be10bd2056676ad9535f66e19d2018330
riyaz <riyazpasha880@gmail.com>
2021-06-14 14:05:58 +0530

    Get all trades

--- src/main/java/com/example/trades/TradesController.java
+++ src/main/java/com/example/trades/TradesController.java
@@ -3,9 +3,12 @@ package com.example.trades;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TradesController {
@@ -17,5 +20,10 @@ public class TradesController {
     public ResponseEntity createTrade(@RequestBody Trade trade) {
         Trade newTrade = tradesRepository.save(trade);
         return ResponseEntity.status(HttpStatus.CREATED).body(newTrade);
+    }
+
+    @GetMapping("/trades")
+    public List<Trade> getTrades() {
+        return tradesRepository.findAll();
     }
 }
