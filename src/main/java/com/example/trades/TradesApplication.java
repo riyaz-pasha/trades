f386c97e4aed199c023926d31f02ac017236a621
riyaz <riyazpasha880@gmail.com>
2021-06-14 10:23:04 +0530

    Initial setup

---
+++ src/main/java/com/example/trades/TradesApplication.java
@@ -0,0 +1,13 @@
+package com.example.trades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradesApplication.class, args);
+	}
+
+}
